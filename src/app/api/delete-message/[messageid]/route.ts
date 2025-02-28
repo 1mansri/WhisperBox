3daf8448106672b3370b8fcee2cdcef009cab6d8
Durgesh Kumar <amansri2023@gmail.com>
2025-02-28 12:48:32 +0530

    Refactor delete message API route: update parameter destructuring and improve error message formatting

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -1,20 +1,21 @@
-import UserModel from '@/models/User.model';
-import { getServerSession } from 'next-auth/next';
-import dbConnect from '@/lib/dbConnect';
-import { User } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]/options';
+import UserModel from "@/models/User.model";
+import { getServerSession } from "next-auth/next";
+import dbConnect from "@/lib/dbConnect";
+import { User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { NextRequest } from "next/server";
 
 export async function DELETE(
-  request: Request,
-  context: { params: { messageid: string } }
+  request: NextRequest,
+  { params }: { params: { messageid: string } }
 ) {
-  const messageId = context.params.messageid;
+  const messageId = params.messageid;
   await dbConnect();
   const session = await getServerSession(authOptions);
   const _user = session?.user as User;
   if (!session || !_user) {
     return Response.json(
-      { success: false, message: 'Not authenticated' },
+      { success: false, message: "Not authenticated" },
       { status: 401 }
     );
   }
@@ -27,19 +28,19 @@ export async function DELETE(
 
     if (updateResult.modifiedCount === 0) {
       return Response.json(
-        { message: 'Message not found or already deleted', success: false },
+        { message: "Message not found or already deleted", success: false },
         { status: 404 }
       );
     }
 
     return Response.json(
-      { message: 'Message deleted', success: true },
+      { message: "Message deleted", success: true },
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error deleting message:', error);
+    console.error("Error deleting message:", error);
     return Response.json(
-      { message: 'Error deleting message', success: false },
+      { message: "Error deleting message", success: false },
       { status: 500 }
     );
   }
